b92f76a952e5dbc2c0f363e81857c34cba9b721e
githubyosshi <kanrisya.shisutemu@gmail.com>
2020-01-30 11:24:46 +0900

    first commit

---
+++ app/src/main/java/com/bird_brown/timerapp01/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bird_brown.timerapp01;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
